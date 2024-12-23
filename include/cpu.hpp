23d833a5665eb4d59f6e507bb54054316280dc55
Akashneel@arch <akashneelrc@gmail.com>
2024-12-23 21:47:58 +0530

    feat: Add CPU functionality, change addressing style
    Add functionality to get and set data, bot 8 and 16 bit. Change data
    storing and addressing scheme to reflect an entire 2048+96 word based
    data address space instead of individual register data spaces.

--- include/cpu.hpp
+++ include/cpu.hpp
@@ -1,13 +1,80 @@
+#pragma once
 #include <stdint.h>
 
 class CPU{
 public:
-    uint8_t REG[32];
     uint16_t SP;
     uint16_t PC;
-    uint8_t SREG;
+
+    uint8_t data[2048];
+
+    static constexpr uint8_t WORD_SIZE= 0xFF;
+
+    static constexpr uint16_t RAMEND= 0x85F;
+    static constexpr uint16_t REG_OFFSET= 0x00;
+    static constexpr uint16_t IOREG_OFFSET= 0x20;
+    static constexpr uint16_t SRAM_OFFSET= 0x60;
 
     CPU();
 
     bool next_tick();
+
+    // Register address space 0x00-0x1F
+    inline uint8_t get_reg(uint8_t addr){
+        return get_data(addr+REG_OFFSET);
+    }
+    inline bool set_reg(uint8_t addr, uint8_t val){
+        return set_data(addr+REG_OFFSET, val);
+    }
+
+    // IO address space 0x00-0x3F
+    inline uint8_t get_ioreg(uint8_t addr){
+        return get_data(addr+IOREG_OFFSET);
+    }
+    inline bool set_ioreg(uint8_t addr, uint8_t val){
+        return set_data(addr+IOREG_OFFSET, val);
+    }
+
+    // Interal SRAM addresses 0x00-0x85
+    inline uint8_t get_sram(uint8_t addr){
+        return get_data(addr+SRAM_OFFSET);
+    }
+    inline bool set_sram(uint8_t addr, uint8_t val){
+        return set_data(addr+SRAM_OFFSET, val);
+    }
+
+    // Data address space 0x00-0x85F (Registers 0x00-0x1F, IO Registers 0x20-0x5F, SRAM 0x60-0x85F)
+    uint8_t get_data(uint8_t);
+    bool set_data(uint8_t, uint8_t);
+
+    // functions for 16 bit addressing, using the higher byte data address
+
+    // Register address space 0x00-0x1F
+    inline uint16_t wget_reg(uint8_t addr){
+        return wget_data(addr+REG_OFFSET);
+    }
+    inline bool wset_reg(uint8_t addr, uint16_t val){
+        return wset_data(addr+REG_OFFSET, val);
+    }
+
+    // IO address space 0x00-0x3F
+    inline uint16_t wget_ioreg(uint8_t addr){
+        return wget_data(addr+IOREG_OFFSET);
+    }
+    inline bool wset_ioreg(uint8_t addr, uint16_t val){
+        return wset_data(addr+IOREG_OFFSET, val);
+    }
+
+    // Interal SRAM addresses 0x00-0x85
+    inline uint16_t wget_sram(uint8_t addr){
+        return wget_data(addr+SRAM_OFFSET);
+    }
+    inline bool wset_sram(uint8_t addr, uint16_t val){
+        return wset_data(addr+SRAM_OFFSET, val);
+    }
+
+    // Data address space 0x00-0x85F (Registers 0x00-0x1F, IO Registers 0x20-0x5F, SRAM 0x60-0x85F)
+    uint16_t wget_data(uint8_t);
+    bool wset_data(uint8_t, uint16_t);
+
 };
--- src/cpu.cpp
+++ src/cpu.cpp
@@ -4,6 +4,36 @@ CPU::CPU(){
     PC= 0;
 }
 
+uint8_t CPU::get_data(uint8_t addr){
+    if(addr> RAMEND){
+        return 0x00; // Do something
+    }
+    return this->data[addr];
+}
+
+bool CPU::set_data(uint8_t addr, uint8_t val){
+    if(addr> RAMEND){
+        return false; // Do something
+    }
+
+    this->data[addr]= val;
+}
+
+uint16_t CPU::wget_data(uint8_t addr){
+    uint8_t high, low;
+    low= get_data(addr+WORD_SIZE);
+    high= get_data(addr);
+
+    uint16_t res= 0;
+    res|= low;
+    res|= high<<WORD_SIZE;
+}
+
+bool CPU::wset_data(uint8_t addr, uint16_t val){
+    set_data(addr+WORD_SIZE, (uint8_t)val);
+    set_data(addr, (uint8_t)(val>>WORD_SIZE));
+}
+
 bool CPU::next_tick(){
     return ++PC;
 }
